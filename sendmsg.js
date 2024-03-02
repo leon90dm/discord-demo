39b4e517433bd92e93e8bd34ecebab7a81fb4d4e
Glitch (roan-sparkly-helenium) <none>
2024-03-02 16:47:24 +0000

    🎈💕 Checkpoint ./sendmsg.js:65561069/26942

--- sendmsg.js
+++ sendmsg.js
@@ -109,40 +109,45 @@ const SEND_TOKEN = process.env.SEND_TOKEN;
 
 // Function to send a message to the Discord bot
 export async function SendMessage(channelId, message) {
-  try {
-    // Make an HTTP POST request to the Discord bot API  
-// https://discord.com/api/v9/channels/1213079139125436478/messages
-    const response = await axios.post(`https://discord.com/api/v9/channels/${channelId}/messages`, {
-      content: message,
-    }, {
-      headers: {
-        'Authorization': SEND_TOKEN,
-        'Content-Type': 'application/json',
-      },
-    });
+    try {
+        // Make an HTTP POST request to the Discord bot API  
+        // https://discord.com/api/v9/channels/1213079139125436478/messages
+        const response = await axios.post(`https://discord.com/api/v9/channels/${channelId}/messages`, {
+            content: message,
+        }, {
+            headers: {
+                'Authorization': SEND_TOKEN,
+                'Content-Type': 'application/json',
+            },
+        });
 
-    // Wait for the bot's response 
-    const messageId = response.data.id;
-    console.log('messageId:', messageId);
+        // Wait for the bot's response 
+        const messageId = response.data.id;
+        console.log('messageId:', messageId);
 
-    const responseUrl = `https://discord.com/api/v9/channels/${channelId}/messages?after=${messageId}`;
-    let botResponse;
-    let i = 20;
-    while (i-- > 0) {
-      botResponse = await axios.get(responseUrl, {
-        headers: {
-          'Authorization': 'Bot ' + botToken,
-        },
-      });
-      for (const message of botResponse.data) {
-        if (message.referenced_message && message.referenced_message.id === messageId) {
-          console.log('message response:',JSON.stringify(message, null, 2));
+
+        let botResponse;
+        let currentMessageId = messageId;
+        let i = 20;
+        while (i-- > 0) {
+            const responseUrl = `https://discord.com/api/v9/channels/${channelId}/messages?after=${currentMessageId}`;
+            botResponse = await axios.get(responseUrl, {
+                headers: {
+                    'Authorization': 'Bot ' + botToken,
+                },
+            });
+            for (const message of botResponse.data) {
+                currentMessageId = message.id;
+                if (message.referenced_message && message.referenced_message.id === messageId) {
+                    if (message.components && message.components.length > 0) {
+                        return message.content;
+                    }
+                }
+            }
+            await new Promise(resolve => setTimeout(resolve, 3000));
         }
-      }
-      await new Promise(resolve => setTimeout(resolve, 3000));
+        console.log("end----")
+    } catch (error) {
+        console.error(error);
     }
-    console.log("end-----")
-  } catch (error) {
-    console.error(error);
-  }
 }
