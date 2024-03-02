437c7a0449db4b3e8557e4922902a1a68ab124bc
Glitch (roan-sparkly-helenium) <none>
2024-03-02 12:35:29 +0000

    🎾🙇 Checkpoint ./sendmsg.js:65561069/329

--- sendmsg.js
+++ sendmsg.js
@@ -1,22 +1,25 @@
 import axios from 'axios'; //
 
 const botToken = process.env.CDP_TOKEN; // Replace with your Discord bot token
+const SEND_TOKEN = process.env.SEND_TOKEN;
 
 // Function to send a message to the Discord bot
 export async function SendMessage(channelId, message) {
   try {
-    // Make an HTTP POST request to the Discord bot API
-    const response = await axios.post(`https://discord.com/api/channels/${channelId}/messages`, {
+    // Make an HTTP POST request to the Discord bot API  
+// https://discord.com/api/v9/channels/1213079139125436478/messages
+    const response = await axios.post(`https://discord.com/api/v9/channels/${channelId}/messages`, {
       content: message,
     }, {
       headers: {
-        'Authorization': 'Bot ' + botToken,
+        'Authorization': SEND_TOKEN,
         'Content-Type': 'application/json',
       },
     });
 
-    // Wait for the bot's response
+    // Wait for the bot's response 
     const messageId = response.data.id;
+    console.log('messageid:' +messageId)
     const responseUrl = `https://discord.com/api/channels/${channelId}/messages/${messageId}`;
     const botResponse = await axios.get(responseUrl, {
       headers: {
