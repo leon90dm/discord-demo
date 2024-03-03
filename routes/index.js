c5cef576af5ad58a4de75132147ae7209612e3d5
daimao <daimao@didiglobal.com>
2024-03-03 12:32:41 +0800

    hk

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+import {SendMessage} from './sendmsg.js';
+const CHANNEL_ID = process.env.CHANNEL_ID;
+const POT_ID = process.env.POT_ID
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/chat', async (req, res) => {
+  const msg = req.query.msg
+  console.log('query msg:'+msg)
+  res.send(await SendMessage(CHANNEL_ID, `<@${POT_ID}> ${msg}`));
 });
 
 module.exports = router;
---
+++ sendmsg.js
@@ -0,0 +1,48 @@
+import axios from 'axios'; 
+
+const botToken = process.env.CDP_TOKEN; // Replace with your Discord bot token
+const SEND_TOKEN = process.env.SEND_TOKEN;
+
+// Function to send a message to the Discord bot
+export async function SendMessage(channelId, message) {
+    try {
+        // Make an HTTP POST request to the Discord bot API  
+        // https://discord.com/api/v9/channels/1213079139125436478/messages
+        const response = await axios.post(`https://discord.com/api/v9/channels/${channelId}/messages`, {
+            content: message,
+        }, {
+            headers: {
+                'Authorization': SEND_TOKEN,
+                'Content-Type': 'application/json',
+            },
+        });
+
+        // Wait for the bot's response 
+        const messageId = response.data.id;
+        console.log('messageId:', messageId);
+
+        let botResponse;
+        let i = 20;
+        while (i-- > 0) {
+            const responseUrl = `https://discord.com/api/v9/channels/${channelId}/messages?after=${messageId}`;
+            botResponse = await axios.get(responseUrl, {
+                headers: {
+                    'Authorization': 'Bot ' + botToken,
+                },
+            });
+            for (const message of botResponse.data) {
+                if (message.referenced_message && message.referenced_message.id === messageId) {
+                    //log(message.content);
+                    console.log(message.id,":",":",message.components.length,"->",message.content);
+                    if (message.components && message.components.length > 0) {
+                        return message.content;
+                    }
+                }
+            }
+            await new Promise(resolve => setTimeout(resolve, 3000));
+        }
+        console.log("end----")
+    } catch (error) {
+        console.error(error);
+    }
+}
