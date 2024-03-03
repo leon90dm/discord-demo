8fb93d40d1ce6ce6395aff493c53af8acdd1d083
daimao <daimao@didiglobal.com>
2024-03-03 15:06:36 +0800

    op

---
+++ routes/dschat.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+const {SendMessage} =require('../sendmsg.js');
+const CHANNEL_ID = process.env.CHANNEL_ID;
+const POT_ID = process.env.POT_ID
+
+router.get('/', async function (req, res, next) {
+  const msg = req.query.msg
+  console.log('query msg:'+msg)
+  var resMsg = await SendMessage(CHANNEL_ID, `<@${POT_ID}> ${msg}`);
+  res.render('chat', { title: 'Chat', msg: resMsg });
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const {SendMessage} =require('./sendmsg.js');
-const CHANNEL_ID = process.env.CHANNEL_ID;
-const POT_ID = process.env.POT_ID
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.get('/chat', async function (req, res, next) {
-  const msg = req.query.msg
-  console.log('query msg:'+msg)
-  var resMsg = await SendMessage(CHANNEL_ID, `<@${POT_ID}> ${msg}`);
-  res.render('chat', { title: 'Chat', msg: resMsg });
 });
 
 module.exports = router;
