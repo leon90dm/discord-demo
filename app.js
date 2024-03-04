f73a166ee05d4ad73e869f27c75e2b05c6cabf0e
bilbodai <bilbodai@163.com>
2024-03-04 15:12:01 +0800

    orders

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/chat', dschatRouter);
 app.use('/users', usersRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
